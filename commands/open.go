b4e359d80b569288f3cedada789afcb58069112c
robhurring <robhurring@gmail.com>
2015-03-03 16:25:49 -0500

    add open command

---
+++ commands/open.go
@@ -0,0 +1,27 @@
+package commands
+
+import (
+	"github.com/codegangsta/cli"
+	"github.com/codeskyblue/go-sh"
+	"github.com/robhurring/jit/jit"
+	"github.com/robhurring/jit/ui"
+)
+
+func init() {
+	CmdRunner.Add(&cli.Command{
+		Name:      "open",
+		ShortName: "o",
+		Usage:     "Open the [ISSUE] in the browser",
+		Action: func(c *cli.Context) {
+			key, err := jit.FindIssueKey(c.Args())
+
+			if err != nil {
+				ui.Errorln(err)
+			} else {
+				url := jit.IssueURL(key)
+				ui.Printf("@{!w}Opening@| %s\n", url)
+				sh.Command("open", url).Run()
+			}
+		},
+	})
+}
